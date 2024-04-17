9f6cad5a9ccbacf70f6661127da32887726aa858
kamo4730 <105842619+kamo4730@users.noreply.github.com>
2024-04-17 15:59:41 +0900

    Add files via upload

--- 1.js
+++ 1.js
@@ -2,14 +2,24 @@ const apikey = "1be95c0eddedeab7f024f53d4f52fb6a"
 
 let city = "Suwon"
 
-const url = 'https://api.openweathermap.org/data/2.5/weather?q=$(city)appid=$(apikey)'
+const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}`
 
 async function getWeather() {
     try {
         const response = await axios.get(url)
+        const weather = {
+            city: response.data.name,
+            tempurature: response.data.main.temp,
+            humidity: response.data.main/humidity
+        };
+        let displayTempurature = Math.floor10(weather.tempurature, -1)
 
-        console.log(response)
+        document.getElementById('output').innerHTML = `도시:${weather.city}, 온도:${displayTempurature}, 습도:${weather.humidity}`
+        console.log(`도시:${weather.city}, 온도:${displayTempurature}, 습도:${weather.humidity}`)
+
     } catch (error) {
 
     }
 }
+getWeather();
+console.log('hi')
